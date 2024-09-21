d7a58be6d1cdc176a2711881e660fd9b6e50758b
Gamarrawq21r <Briangamarra829@gmail.com>
2024-09-20 21:14:24 -0400

    Update 04.py

--- 04.py
+++ 04.py
@@ -36,16 +36,16 @@ def point(x, y):
     color('black'), fillcolor('#FFA216')
     begin_fill(), circle(4), end_fill()
 
-# Function to draw 'T'
-def draw_T(x, y):
+# Function to draw 'Bren'
+def draw_Bren(x, y):
     positions_t = [(x, y+30), (x+6, y+30), (x+12, y+30), (x+18, y+30), (x+24, y+30),
                    (x+12, y+30), (x+12, y+24), (x+12, y+18), (x+12, y+12), (x+12, y+6), (x+12, y)]
 
     for pos in positions_t:
         point(*pos)
 
-# Function to draw 'Ú'
-def draw_U(x, y):
+# Function to draw 'da'
+def draw_da(x, y):
     positions_u = [(x, y+30), (x, y+24), (x, y+18), (x, y+12), (x, y+6),
                    (x+3, y+3), (x+6, y), (x+12, y-1), (x+18, y), (x+21, y+3),
                    (x+24, y+6), (x+24, y+12), (x+24, y+18), (x+24, y+24), (x+24, y+30),
@@ -54,9 +54,9 @@ def draw_U(x, y):
     for pos in positions_u:
         point(*pos)
 
-# Draw 'TÚ'
-draw_T(-27, -20)
-draw_U(7, -20)
+# Draw 'Brenda'
+draw_Bren(-27, -20)
+draw_da(7, -20)
 
 hideturtle()
 done()
